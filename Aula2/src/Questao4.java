36bcad005755cca0828d67855e3a3a145d5fa5ad
joaohenriquecf <joaohenriquecastrofonseca@gmail.com>
2024-08-27 10:03:32 -0300

    Segundo commit

--- Aula2/src/Questao4.java
+++ Aula2/src/Questao4.java
@@ -1,5 +1,21 @@
 public class Questao4 {
     public static void main(String[] args) {
-        
+
+        double salario = 1250;
+
+        if (salario <= 900) {
+            System.out.println("Isento");
+
+        } else if (salario >= 901 || salario <= 1500) {
+            System.out.println("desconto de 5%");
+
+        } else if (salario >= 1501  || salario <= 2500) {
+            System.out.println("Desconto de 10%");
+
+        } else{
+            System.out.println("desconto de 20%");
+        }
+
+
     }
 }
---
+++ aula3/src/TESTE.java
@@ -0,0 +1,11 @@
+public class TESTE {
+    public static void main(String[] args) {
+        int x= 0;
+        switch (x){
+            case 0:
+                System.out.println("Opção 0 escolhida");
+                break;
+
+        }
+    }
+}
